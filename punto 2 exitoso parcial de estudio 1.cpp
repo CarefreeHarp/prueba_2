526b527a87e8d3a00d6f949abef042fd527ffc0d
Daniel <daniramirezva@gmail.com>
2024-03-23 23:58:12 -0500

    nuevo codigo

---
+++ punto 2 exitoso parcial de estudio 1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+main() 
+{
+   int horadesalida,pasajerosaca=0,pasajerost,chiva,van,campero;
+   	cout<<"Hora de salida   |   Pasajeros transportados   |   Pasajeros en espera   |   Vehiculo(s) Utilizado(s)   |"<<endl;
+	cout<<"                 |                             |                         |   Chiva  |  Van  |  Campero  |"<<endl;
+   for(horadesalida=5;horadesalida<=17;horadesalida++)
+   {	
+        chiva=0;
+   	    van=0;
+   	    campero=0;
+   	    pasajerost=0;
+		pasajerosaca=(horadesalida*horadesalida)-(24*horadesalida)+144+pasajerosaca;
+		if(pasajerosaca>=21)
+		{
+			chiva++;
+			pasajerosaca=pasajerosaca-21;
+			pasajerost=pasajerost+21;
+		}
+		if(pasajerosaca>=11)
+		{
+			van++;
+			pasajerosaca=pasajerosaca-11;
+			pasajerost=pasajerost+11;
+		}
+		if(pasajerosaca>=7)
+		{
+			campero++;
+			pasajerosaca=pasajerosaca-7;
+			pasajerost=pasajerost+7;
+		}
+		cout<<horadesalida<<":00 | pasajeros transportados: "<<pasajerost<<" | pasajeros en espera: "<<pasajerosaca<<" | chivas: "<<chiva<<" | Vans: "<<van<<" | Camperos: "<<campero<<" |"<<endl;
+   }
+}
